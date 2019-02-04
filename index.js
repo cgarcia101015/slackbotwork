b3d224376cf37ffae4fcb5b180f794fe39a3478b
Carlos M Garcia <cgarcia101015@gmail.com>
2019-02-04 16:34:57 -0500

    added GeoLoc function

--- index.js
+++ index.js
@@ -1,111 +1,145 @@
-require("dotenv").config();
+require('dotenv').config();
 var keys = require('./keys.js');
-SlackBot = require("slackbots");
-axios = require("axios");
+SlackBot = require('slackbots');
+axios = require('axios');
+var NodeGeocoder = require('node-geocoder');
 
-console.log('these are the keys');
-console.log(keys.zomato);
 // Setup our slackbot with the app we created and their credentials
 var bot = new SlackBot({
-    token: process.env.SLACKBOT_TOKEN,
-    name: "work_meal"
+	token: process.env.SLACKBOT_TOKEN,
+	name: 'work_meal'
 });
 
 var zamKey = keys.zomato;
 var edaKey = keys.edamam;
 var firKey = keys.firebase;
 
-
-// var zomKey = process.env.ZOMATO_KEY;
-
-// var edaKey = process.env.EDAMAN_KEY;
-
 // Error Handler
-bot.on("error", function (error) {
-    console.log("Uh-oh, slackBot error: " + error);
+bot.on('error', function(error) {
+	console.log('Uh-oh, slackBot error: ' + error);
 });
 
 // Start Handler - starts the slackbot
 // Event = start - triggered when the bot is successfully connected to the Slack server
-bot.on("start", function () {
-    // this makes the emoji of the slackbot a pizza
-    var params = {
-        icon_emoji: ":pizza:"
-    };
-    // Post a message into specified slack channel
-    bot.postMessageToChannel("slack-bot-for-meals", "What would you like for lunch today?", params);
+bot.on('start', function() {
+	// this makes the emoji of the slackbot a pizza
+	var params = {
+		icon_emoji: ':pizza:'
+	};
+	// Post a message into specified slack channel
+	bot.postMessageToChannel('slack-bot-for-meals', 'What would you like for lunch today?', params);
 });
 
-// Mention Handler - someone mentions the app 
-bot.on("message", function (data) {
-    var userInput = data.text;
-    // If the bot sent the message, do nothing 
-    if (data.type !== "message" || data.subtype === "bot_message" || userInput == "undefined") {
-        return;
-        // If the message calls out the work meal bot specifically, and says nothing else, respond
-    } else if (data.text === "<@UFR15N7TQ>") {
-        bot.postMessageToChannel("slack-bot-for-meals", "Yes, how can I help you?", {
-            "slackbot":
-                true, icon_emoji: ":question:"
-        });
-        // If a message was sent that has any other content and is not from the workbot, call the recipes API
-    } else if (data.type === "message" && data.username != "work_meal" && data.text !== undefined && data.channel === "CFNLZH407") {
-        // turn multiple word requests into string for recipe API to search
-        var apiInput = userInput.replace(/ /g, "+");
-        console.log("A message was sent that said: " + userInput);
-        console.log("The API will search for: " + apiInput);
-        returnLunch();
-        returnRestaurant();
-    }
+// Mention Handler - someone mentions the app
+bot.on('message', function(data) {
+	var userInput = data.text;
+	// If the bot sent the message, do nothing
+	if (data.type !== 'message' || data.subtype === 'bot_message' || userInput == 'undefined') {
+		return;
+		// If the message calls out the work meal bot specifically, and says nothing else, respond
+	} else if (data.text === '<@UFR15N7TQ>') {
+		bot.postMessageToChannel('slack-bot-for-meals', 'Yes, how can I help you?', {
+			slackbot: true,
+			icon_emoji: ':question:'
+		});
+		// If a message was sent that has any other content and is not from the workbot, call the recipes API
+	} else if (
+		data.type === 'message' &&
+		data.username != 'work_meal' &&
+		data.text !== undefined &&
+		data.channel === 'CFNLZH407'
+	) {
+		// turn multiple word requests into string for recipe API to search
+		var apiInput = userInput.replace(/ /g, '+');
+		console.log('A message was sent that said: ' + userInput);
+		console.log('The API will search for: ' + apiInput);
+		returnLunch();
+		returnRestaurant();
+		findLocation();
+	}
+	function findLocation() {
+		var options = {
+			provider: 'mapquest',
 
-    function returnRestaurant() {
-        var config = { headers: { "user-key": zamKey } };
-        var URL = "https://developers.zomato.com/api/v2.1/search?q=" + userInput + "&lat=40.809498&lon=-73.960154&radius=500%&sort=real_distance";
+			// Optional depending on the providers
+			httpAdapter: 'https', // Default
+			apiKey: 'B4JrLqy7Tg3efG2mqQJDfiiOG1hnI9h0', // for Mapquest, OpenCage, Google Premier
+			formatter: null // 'gpx', 'string', ...
+		};
+		var geocoder = NodeGeocoder(options);
 
-        axios.get(URL, config).then(function (response) {
-            var jsonData = response.data;
-            var showData = [
-                "Name: " + jsonData.restaurants[1].restaurant.name,
-                "Location: " + jsonData.restaurants[1].restaurant.location.address,
-                "Url to Restaurant: " + jsonData.restaurants[1].restaurant.url,
-            ].join("\n\n");
-            // console.log(response);
-            // console.log(zomato);
-            var params = {
-                icon_emoji: ":bread:"
-            };
-            bot.postMessageToChannel("slack-bot-for-meals", "Here\'s an idea: " + showData, params);
-        }).catch(function (error) {
-            console.log("Zomato API error: " + error);
-            bot.postMessageToChannel("slack-bot-for-meals", "I\'m sorry, I didn\'t get any results for that, could you be more specific?", { "slackbot": true, icon_emoji: ":question:" });
-        });
-    };
+		//Using callback
+		geocoder.geocode('10467', function(err, res) {
+			var geoLat = res[0].latitude;
+			var geoLon = res[0].longitude;
+			console.log('Latitude: ' + geoLat);
+			console.log('Longitude: ' + geoLon);
+		});
+	}
 
+	function returnRestaurant() {
+		var config = { headers: { 'user-key': zamKey } };
+		var URL =
+			'https://developers.zomato.com/api/v2.1/search?q=' +
+			userInput +
+			'&lat=40.809498&lon=-73.960154&radius=500%&sort=real_distance';
 
-    // Return a recipe 
-    function returnLunch() {
-        axios.get("https://api.edamam.com/search?q=" + apiInput + "&app_id=45d6973d&app_key=" + edaKey).then(function (response) {
-            var jsonData = response.data;
+		axios
+			.get(URL, config)
+			.then(function(response) {
+				var jsonData = response.data;
+				var showData = [
+					'Name: ' + jsonData.restaurants[1].restaurant.name,
+					'Location: ' + jsonData.restaurants[1].restaurant.location.address,
+					'Url to Restaurant: ' + jsonData.restaurants[1].restaurant.url
+				].join('\n\n');
+				// console.log(response);
+				// console.log(zomato);
+				var params = {
+					icon_emoji: ':bread:'
+				};
+				bot.postMessageToChannel('slack-bot-for-meals', "Here's an idea: " + showData, params);
+			})
+			.catch(function(error) {
+				console.log('Zomato API error: ' + error);
+				bot.postMessageToChannel(
+					'slack-bot-for-meals',
+					"I'm sorry, I didn't get any results for that, could you be more specific?",
+					{ slackbot: true, icon_emoji: ':question:' }
+				);
+			});
+	}
 
-            var showData = [
-                jsonData.q,
-                "Title: " + jsonData.hits[1].recipe.label,
-                "Image: " + jsonData.hits[1].recipe.image,
-                "Url to Recipe: " + jsonData.hits[1].recipe.url,
-            ].join("\n\n");
+	// Return a recipe
+	function returnLunch() {
+		axios
+			.get('https://api.edamam.com/search?q=' + apiInput + '&app_id=45d6973d&app_key=' + edaKey)
+			.then(function(response) {
+				var jsonData = response.data;
 
-            var params = {
-                icon_emoji: ":bread:"
-            };
-            bot.postMessageToChannel("slack-bot-for-meals", "Here\'s an idea: " + showData, params);
-        }).catch(function (error) {
-            console.log("Edamam API error: " + error);
-            bot.postMessageToChannel("slack-bot-for-meals", "I\'m sorry, I didn\'t get any results for that, could you be more specific?", { "slackbot": true, icon_emoji: ":question:" });
-        });
-    }
+				var showData = [
+					jsonData.q,
+					'Title: ' + jsonData.hits[1].recipe.label,
+					'Image: ' + jsonData.hits[1].recipe.image,
+					'Url to Recipe: ' + jsonData.hits[1].recipe.url
+				].join('\n\n');
 
-    // function rude() {
-    //     bot.postMessageToChannel("slack-bot-for-meals", "Well that\'s rude.", { "slackbot": true, icon_emoji: ":neutral_face:"});
-    // }
+				var params = {
+					icon_emoji: ':bread:'
+				};
+				bot.postMessageToChannel('slack-bot-for-meals', "Here's an idea: " + showData, params);
+			})
+			.catch(function(error) {
+				console.log('Edamam API error: ' + error);
+				bot.postMessageToChannel(
+					'slack-bot-for-meals',
+					"I'm sorry, I didn't get any results for that, could you be more specific?",
+					{ slackbot: true, icon_emoji: ':question:' }
+				);
+			});
+	}
 
+	// function rude() {
+	//     bot.postMessageToChannel("slack-bot-for-meals", "Well that\'s rude.", { "slackbot": true, icon_emoji: ":neutral_face:"});
+	// }
 });
--- keys.js
+++ keys.js
@@ -2,25 +2,10 @@ console.log('this is loaded');
 require('dotenv');
 
 theKeys = {
-    slackBot: process.env.SLACKBOT_TOKEN  ,
-    zomato: process.env.ZOMATO_USER_KEY,
-    edamam: process.env.REACT_APP_APP_KEY,
-    firebase: process.env.FIREBASE_KEY
-}
-
-
-// slackbot = {
-//     key: process.env.SLACKBOT_TOKEN
-// },
-// zomato = {
-//      key: process.env.ZOMATO_KEY
-// },
-// edaman = {
-//     key: process.env.EDAMAN_KEY
-// },
-// firebase = {
-//     key: process.env.FIREBASE_KEY
-// }
+	slackBot: process.env.SLACKBOT_TOKEN,
+	zomato: process.env.ZOMATO_USER_KEY,
+	edamam: process.env.REACT_APP_APP_KEY,
+	firebase: process.env.FIREBASE_KEY
+};
 
 module.exports = theKeys;
-// console.log(keys);
